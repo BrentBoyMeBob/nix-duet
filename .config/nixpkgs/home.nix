7d2aead33964bd9a93e2840a641c927689422985
BrentBoyMeBob <brent.monning.jr@gmail.com>
2021-11-18 14:29:15 -0800

    nix-duet: configured and installed vim

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -14,6 +14,38 @@
     pkgs.zsh
   ];
 
+  # Configure Vim..
+  programs.vim = {
+    # Enable it first.
+    enable = true;
+
+    # Add some plugins.
+    plugins = with pkgs.vimPlugins; [ 
+      lightline-vim
+      nord-vim
+      nerdtree
+      rainbow
+    ];
+
+    # Configure it through Vim script.
+    extraConfig = ''
+      " Disable mouse input because it messes with the Crostini terminal.
+      set mouse=
+
+      " Enable the Nord theme along with the lightline theme.
+      colorscheme nord
+      let g:lightline = {
+        \ 'colorscheme': 'nord',
+        \ }
+
+      " Disable case sensitivity.
+      set ignorecase
+
+      " Enable Rainbow brackets.
+      let g:rainbow_active = 1
+    '';
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
